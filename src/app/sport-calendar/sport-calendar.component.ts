834404b9f409b5ef7804e8b011872f24a580bb4a
Auguste Janickaite <auguste@Augustes-MBP.lan>
2023-02-17 12:12:17 +0200

    Auto-generated commit [ci skip]

--- src/app/sport-calendar/sport-calendar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sport-calendar',
-  templateUrl: './sport-calendar.component.html',
-  styleUrls: ['./sport-calendar.component.scss'],
-})
-export class SportCalendarComponent {}
